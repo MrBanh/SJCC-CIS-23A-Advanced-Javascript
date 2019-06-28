04b38de3af89087cf8255bf791bb6db10250ae32
DESKTOP-0P21BI7\Tony B <25971310+MrBanh@users.noreply.github.com>
2019-06-28 01:21:31 -0700

    chore: write down requirements to complete assignment # 2

--- scripts/Assignment2.js
+++ scripts/Assignment2.js
@@ -1,5 +1,9 @@
 const createEventListeners = () => {
     console.log("Test2");
+    // TODO: Change the page background color for every click of a button using js
+    // TODO: Create a rectangle or circular shape stop-watch (start/stop timer) with 24 hours max duration
+    // TODO: https://www.youtube.com/watch?v=1INmsFnD-u4
+    // TODO: Take output screen shots and upload with associated source code
 }
 
 export {
