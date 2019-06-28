caefcbaab7d72219d63595a3f1aed41f4fefaf4c
DESKTOP-0P21BI7\Tony B <25971310+MrBanh@users.noreply.github.com>
2019-06-28 02:47:32 -0700

    feat: Assignment 2, add feature to change background color with each click

--- scripts/Assignment2.js
+++ scripts/Assignment2.js
@@ -1,6 +1,17 @@
+const randomBackgroundColor = () => {
+    let red = Math.ceil(Math.random() * 255);
+    let green = Math.ceil(Math.random() * 255);
+    let blue = Math.ceil(Math.random() * 255);
+
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+
 const createEventListeners = () => {
-    console.log("Test2");
-    // TODO: Change the page background color for every click of a button using js
+    // Change the page background color for every click of a button using js
+    const bodyTag = document.querySelector("body");
+    document.addEventListener("click", () => {
+        bodyTag.style.background = randomBackgroundColor();
+    });
     // TODO: Create a rectangle or circular shape stop-watch (start/stop timer) with 24 hours max duration
     // TODO: https://www.youtube.com/watch?v=1INmsFnD-u4
     // TODO: Take output screen shots and upload with associated source code
