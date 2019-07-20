b347c4e3e9b2d36a5781e3b0fbd285e393c18cd4
DESKTOP-0P21BI7\Tony B <25971310+MrBanh@users.noreply.github.com>
2019-07-20 00:24:22 -0700

    Work on assignment 5 most frequent occurrence html and css

--- scripts/Assignment5.js
+++ scripts/Assignment5.js
@@ -1,5 +1,19 @@
 "use strict";
 
+const inputRegex = /\[|\]|'|"|\s*,\s*/g;
+
+function clearDefaults() {
+    this.value = "";
+}
+
+function removeClear() {
+    this.removeEventListener("focus", clearDefaults);
+}
+
+// TODO: Input Page --- handle user input
+
+// TODO: Output Page --- provide buttons to Continue or Exit
+
 const createEventListeners = () => {
     // Access the selectedLi link
     const assignLinksClicked = document.querySelector("#selectedLi");
@@ -8,6 +22,10 @@ const createEventListeners = () => {
     if (assignLinksClicked.textContent !== "Home") {
         document.querySelector("#nav").scrollIntoView();
     }
+
+    const arrayInput = document.querySelector("#array");
+    arrayInput.addEventListener("focus", clearDefaults);
+    arrayInput.addEventListener("blur", removeClear);
 };
 
 window.addEventListener("load", createEventListeners);
